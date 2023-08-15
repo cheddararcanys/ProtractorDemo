6995e10f1bf56f5ab509a51ea3cc61f109059344
cheddararcanys <chedararca@gmail.com>
2023-08-15 11:19:44 +0800

    message

--- tests/calculator.js
+++ tests/calculator.js
@@ -26,18 +26,6 @@ describe('demo calculator tests', function(){
         browser.sleep(5000);
 
     }) 
-
-    it('subtraction test', function(){
-
-        browser.get('http://juliemr.github.io/protractor-demo/');
-
-        element(by.model('firstt')).sendKeys('2');
-        element(by.model('second')).sendKeys('3');
-
-        element(by.css('[ng-click="doAddition()"]')).click();
-        
-        let result = element(by.cssContainingText('.ng-binding', '5'));
-        expect(result.getText()).toEqual('5');
         
         /*browser.driver.ignoreSynchronization = true;
         browser.waitForAngularEnabled(false);
